9b86ee89f3cf397cd194c71070ffeb8cd9dce276
Federico Pazos <federico.pazos@vibia.com>
2021-10-08 20:32:03 +0200

    add terraform.tf

---
+++ terraform.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.26.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.1"
+    }
+  }
+  required_version = ">= 0.14"
+
+  backend "remote" {
+    organization = "fedepazos"
+
+    workspaces {
+      name = "nestjs-realworld-example-app"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_ecr_repository" "ecr" {
+  name                 = "nestjs-realworld-example-app"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
