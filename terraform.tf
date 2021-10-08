66844ff245cbaac5dff95369430d6fcbdd972c13
Federico Pazos <federico.pazos@vibia.com>
2021-10-08 21:07:44 +0200

    add task_definition creation

--- terraform.tf
+++ terraform.tf
@@ -33,6 +33,35 @@ resource "aws_ecr_repository" "ecr" {
   }
 }
 
+resource "aws_ecs_task_definition" "task_definition" {
+  family = "service"
+  container_definitions = jsonencode([
+    {
+      name      = "nestjs-realworld-example-app"
+      image     = aws_ecr_repository.ecr.repository_url
+      cpu       = 1
+      memory    = 512
+      essential = true
+      networkMode = "awsvpc"
+      portMappings = [
+        {
+          containerPort = 3000
+        }
+      ]
+    }
+  ])
+
+  volume {
+    name      = "service-storage"
+    host_path = "/ecs/service-storage"
+  }
+
+  placement_constraints {
+    type       = "memberOf"
+    expression = "attribute:ecs.availability-zone in [us-west-2a, us-west-2b]"
+  }
+}
+
 output "ecr_repository_url" {
   value = aws_ecr_repository.ecr.repository_url
 }
