242ed838377c125a7f2624d1e7b7c8e81560b874
Federico Pazos <federico.pazos@vibia.com>
2021-10-11 19:47:03 +0200

    add missing count property to ecs service

--- main.tf
+++ main.tf
@@ -164,6 +164,7 @@ resource "aws_ecs_task_definition" "task" {
 }
 
 resource "aws_ecs_service" "service" {
+  count           = var.az_count
   name            = "nestjs-realworld-example"
   cluster         = aws_ecs_cluster.cluster.id
   task_definition = aws_ecs_task_definition.task.arn
