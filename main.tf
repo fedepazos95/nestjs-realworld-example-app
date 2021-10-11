9f3c31bf9e557527fdc3d9f44630d5c4692a2583
Federico Pazos <federico.pazos@vibia.com>
2021-10-11 19:37:23 +0200

    fix main.tf format

--- main.tf
+++ main.tf
@@ -74,14 +74,14 @@ resource "aws_default_security_group" "default" {
   ]
 }
 resource "aws_subnet" "public" {
-  count                   = "${var.az_count}"
-  cidr_block              = "${cidrsubnet(aws_vpc.main.cidr_block, 8, var.az_count + count.index)}"
-  availability_zone       = "${data.aws_availability_zones.available.names[count.index]}"
-  vpc_id                  = "${aws_vpc.main.id}"
+  count                   = var.az_count
+  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, var.az_count + count.index)
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  vpc_id                  = aws_vpc.main.id
   map_public_ip_on_launch = true
 
   tags = {
-    Type        = "public"
+    Type = "public"
   }
 }
 
